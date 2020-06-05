ce7ae94a8ec443be0259f69d164eef0ed7412f6a
loiminh <loiho11082015@gmail.com>
2020-06-05 11:05:21 +0700

    done process-bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { CartComponent } from './cart/cart.component';
+import { ProgressBarComponent } from './progress-bar/progress-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CartComponent
+    CartComponent,
+    ProgressBarComponent
   ],
   imports: [
     BrowserModule
